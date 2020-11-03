9e75501e7e307f9c11bb668209b062c2aa3d1710
fahadkhan917 <fk16181@gmail.com>
2020-11-04 01:20:12 +0500

    update

--- app.js
+++ app.js
@@ -1904,7 +1904,19 @@
 
 // }
 
+function  save() {
+    var name=document.getElementById("name");
+    var roll=document.getElementById("roll");
 
+    var student={
+
+    name:name.value,
+    roll:roll.value
+
+    }
+
+    console.log(student)
+}
 
 
 
