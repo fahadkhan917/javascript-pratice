b009be7e1aa8315fb186ef11e6a0b987907b3cdc
fahadkhan917 <fk16181@gmail.com>
2020-11-04 15:36:04 +0500

    Update db.js

--- db.js
+++ db.js
@@ -3,13 +3,15 @@ function abc(){
 var name=document.getElementById("name");
 var roll=document.getElementById("roll");
 
-
+var key=firebase.database().ref('student').push().key
 var student={
 name:name.value,
-roll:roll.value
-
+roll:roll.value,
+key:key
 
 }
-firebase.database().ref('student').set(student)
+// var ran=Math.random()*154448
+firebase.database().ref('student/'+key).set(student)
 }
+
 
