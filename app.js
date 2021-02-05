69d2021eb065ac723821477eec6363ba53bcda92
fahadkhan917 <fk16181@gmail.com>
2021-02-05 16:44:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1979,3 +1979,33 @@
 // document.write("now the value"+a)
 
 
+
+var num=prompt("enter the number");
+if(num){
+
+document.write(num+"x"+"1"+"="+num*1+"<br>")
+document.write(num+"x"+"2"+"="+num*2+"<br>")
+document.write(num+"x"+"3"+"="+num*3+"<br>")
+document.write(num+"x"+"4"+"="+num*4+"<br>")
+document.write(num+"x"+"5"+"="+num*5+"<br>")
+document.write(num+"x"+"6"+"="+num*6+"<br>")
+document.write(num+"x"+"7"+"="+num*7+"<br>")
+document.write(num+"x"+"8"+"="+num*8+"<br>")
+document.write(num+"x"+"9"+"="+num*9+"<br>")
+document.write(num+"x"+"10"+"="+num*10+"<br>")
+}
+
+else{
+    document.write(5+"x"+"1"+"="+5*1+"<br>")
+    document.write(5+"x"+"2"+"="+5*2+"<br>")
+    document.write(5+"x"+"3"+"="+5*3+"<br>")
+    document.write(5+"x"+"4"+"="+5*4+"<br>")
+    document.write(5+"x"+"5"+"="+5*5+"<br>")
+    document.write(5+"x"+"6"+"="+5*6+"<br>")
+    document.write(5+"x"+"7"+"="+5*7+"<br>")
+    document.write(5+"x"+"8"+"="+5*8+"<br>")
+    document.write(5+"x"+"9"+"="+5*9+"<br>")
+    document.write(5+"x"+"10"+"="+5*10+"<br>")
+
+
+}
