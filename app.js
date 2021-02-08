5d316637e7f87c5ee5414aea5f921ec74c4d4fdf
fahadkhan917 <fk16181@gmail.com>
2021-02-08 19:07:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2207,3 +2207,16 @@
 // document.write("incorrect")
 
 // }
+
+
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// document.write(greeting)
+// }
+// else{
+// greeting= "Good evening";
+// document.write(greeting)
+// }
