07a08b90c987ad854b515183c9a2172c8548515b
fahadkhan917 <fk16181@gmail.com>
2021-02-24 14:12:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3057,14 +3057,36 @@ var color=["red","blue","pink","purple","yellow"];
 // document.write(missing)
 
 
-function findMissingNumbers(arr) {
-    // Create sparse array with a 1 at each index equal to a value in the input.
-    var sparse = arr.reduce((sparse, i) => (sparse[i]=1,sparse), []);
-    // Create array 0..highest number, and retain only those values for which
-    // the sparse array has nothing at that index (and eliminate the 0 value).
-    return [...sparse.keys()].filter(i => i && !sparse[i]);
-}
+// function findMissingNumbers(arr) {
+//     // Create sparse array with a 1 at each index equal to a value in the input.
+//     var sparse = arr.reduce((sparse, i) => (sparse[i]=1,sparse), []);
+//     // Create array 0..highest number, and retain only those values for which
+//     // the sparse array has nothing at that index (and eliminate the 0 value).
+//     return [...sparse.keys()].filter(i => i && !sparse[i]);
+// }
 
-var someArr = [2, 5, 3, 1, 4, 7, 10, 15]
-var result = findMissingNumbers(someArr);
-document.write(result);
+// var someArr = [2, 5, 3, 1, 4, 7, 10, 15]
+// var result = findMissingNumbers(someArr);
+// document.write(result);
+
+
+
+// var a=5;
+// for(var i=1;i<10;i++){
+    
+// if(i===a){
+// break;
+
+// }
+// document.write(i)
+// }
+
+// var a=[45,"fahad",98,78,22]
+// for(var i=0;i<a.length;i++){
+
+// if(a.indexOf(i)=="fahad"){
+
+//     document.write("yes")
+// }
+
+// }
